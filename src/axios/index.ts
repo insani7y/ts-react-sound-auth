e58204f4e21043b2fedf3895b076613e9a57bc09
Michael Gurbanov <MYGurbanov@sberbank.ru>
2021-11-30 19:38:20 +0300

    Ready

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8080/",
+    baseURL: `http://0.0.0.0:8000/go/`,
     timeout: 3000,
     headers: {
         "Content-Type": "application/json",
